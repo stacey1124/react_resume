b8ef7eb066790635ca3cc93c03d6aa39a0ea1ca3
yuanshi <shi.yuan@ifchange.com>
2020-11-26 10:42:20 +0800

    第四次提交-遗留问题popup动画效果和display:none冲突

--- src/js/resume/bottom-modal.js
+++ src/js/resume/bottom-modal.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import { findDOMNode } from 'react-dom';
+
 import classnames from 'classnames';
 import '../../css/bottom-modal.css'
 
@@ -12,33 +14,38 @@ export default class BottomModal extends React.Component {
     const visible = false;
     const height = 130;
     this.props.isPageShow({visible, height});
+    const modalCom = document.getElementById("#modalComponent");
+    console.log("modalCom", modalCom)
   }
 
+
+
   render() { 
+    // console.log("this.refs.modalComponent", this.refs.modalComponent)
+    
+
     const { isDelPageVis } = this.props
-    const fade_eo = isDelPageVis ? "pop-fade-enter" : "pop-fade-out";
+    const isModalShow = isDelPageVis ? "block" : "none";
     console.log("this.props.isDelPageVis:", this.props.isDelPageVis)
     let height = isDelPageVis ? 0 : 130;
     const btmStyle = { "transform": "translateY(" + `${height}` + "px)" }
 
     return (
       <Fragment>
-        {isDelPageVis ? (
-        <div>
+       
+        <div style={{display: isModalShow}} ref="modalComponent" id="modalComponent" >
+          <div className={classnames("popup-mask", isDelPageVis ? "pop-fade-enter" : "pop-fade-out")} 
+          >
+          </div>
 
-          <div className={classnames("popup-mask", fade_eo)}></div>
+          <div className="bottom-modal">
+            <div className="delete--wrapper" style={btmStyle}>
+              <div className="delete__resume">删除简历</div>
+              <div className="delete__cancel" onClick={this.handleCancel}>取消</div>
+            </div>
+          </div>
+        </div>
 
-      <div className="bottom-modal">
-        <div className="delete--wrapper" style={btmStyle}>
-          <div className="delete__resume">删除简历</div>
-          <div className="delete__cancel" onClick={this.handleCancel}>取消</div>
-        </div>
-      </div>
-        </div>
-        )
-         : ''
-        }
-      
       </Fragment>
 
     )
