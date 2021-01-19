62c1627dd13d44394bcfdb86710186c38aa6a500
stacey1124 <1174452667@qq.com>
2021-01-19 10:26:33 +0800

    切换分支提交

--- src/js/React16/MenuWrapper/Email/index.js
+++ src/js/React16/MenuWrapper/Email/index.js
@@ -1,10 +1,29 @@
 import React, { Fragment } from 'react'
+import { Button } from 'antd'
 
 export default class Email extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      visible: true
+    })
+  }
   render() {
+    const { visible } = this.state
     return (
       <Fragment>
         <h1>邮件</h1>
+        <Button onClick={this.handleClick}></Button>
+        {visible ? 
+          (<div className="">
+            
+          </div>) 
+        : null}
       </Fragment>
     )
   }
